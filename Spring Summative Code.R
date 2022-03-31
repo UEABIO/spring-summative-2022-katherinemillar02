0ca53dd58594577f8758056f6332e13cd2fbe207
katherinemillar02 <ajh20fhu@uea.ac.uk>
2022-03-31 15:43:25 +0000

    Removed the previous code for datasets and added read_excel to have a path to view the separate sheets withtin the one dataset.

--- Spring Summative Code.R
+++ Spring Summative Code.R
@@ -1,40 +1,17 @@
 library(tidyverse)
 library(readxl)
 
+elegansdata <- read_excel("Data/elegans.xlsx")
+readxl::read_excel("Data/elegans.xlsx")
 excel_sheets("Data/elegans.xlsx")
+read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f0")
+read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1")
+
+head(read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1"))
+head(read_excel(path = "Data/elegans.xlsx", sheet = "lifespan_f0"))
 
 
-readxl::read_excel
 
-celegans <- read.csv("Data/elegans_data.csv")
-reproductionf0 <- read.csv("Data/reproduction_f0.csv")
-lifespanf0 <- read.csv("Data/lifespan_f0.csv")
-# f0 = parental generation 
-reproductionf1 <- read.csv("Data/reproduction_f1.csv")
-# f1 = offspring 
-
-head(celegans)
-head(reproductionf0)
-colnames(celegans)
-summary(celegans)
-
-celegans %>% 
-  duplicated() %>% 
-  sum()
-
-celegans %>%
-summarise(min=min(death_date, na.rm=TRUE), 
-          max=max(death_date, na.rm=TRUE))
-
-celegans%>% 
-  distinct(parental_rnai)
-
-celegans%>%
-  distinct(worm)
-
-celegans%>%
-  is.na()
-sum()
 
 
 
