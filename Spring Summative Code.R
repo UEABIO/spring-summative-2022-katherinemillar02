3c229143ed6776bf9f9db657d4aca7f489730f7c
katherinemillar02 <ajh20fhu@uea.ac.uk>
2022-04-04 14:25:47 +0000

    Worked to visualise the data Attempted to look at difference in dates - longevity for the c.elegans

--- Spring Summative Code.R
+++ Spring Summative Code.R
@@ -1,19 +1,31 @@
 library(tidyverse)
 library(readxl)
 
+#Read Data in
 elegansdata <- read_excel("Data/elegans.xlsx")
 readxl::read_excel("Data/elegans.xlsx")
 excel_sheets("Data/elegans.xlsx")
+
+#The different sheets 
 read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f0")
 read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1")
+read_excel(path = "Data/elegans.xlsx", sheet = "lifespan_f0")
+read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan")
 
-head(read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1"))
+
+#Checking the data
+summary(read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1"))
 head(read_excel(path = "Data/elegans.xlsx", sheet = "lifespan_f0"))
+head(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan"))
+head(read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f0"))
 
 
-
-
-
+# Visualising the data 
+(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan")) %>% 
+  ggplot(aes(x=treatment,
+             y=plate))+
+  geom_point()
+     
 
 
 
