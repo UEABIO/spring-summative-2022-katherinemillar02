0eb42b6746b06791b5325ba4a75c5565e4b14688
katherinemillar02 <ajh20fhu@uea.ac.uk>
2022-04-07 12:58:08 +0000

    Attempt to change the number values back to dates.

--- Spring Summative Code.R
+++ Spring Summative Code.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
+library(kableExtra)
 
 #Read Data in
 elegansdata <- read_excel("Data/elegans.xlsx")
@@ -12,20 +13,42 @@ read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1")
 read_excel(path = "Data/elegans.xlsx", sheet = "lifespan_f0")
 read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan")
 
-
 #Checking the data
 summary(read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1"))
 head(read_excel(path = "Data/elegans.xlsx", sheet = "lifespan_f0"))
 head(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan"))
 head(read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f0"))
-
+glimpse(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan"))
+summary(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan"))
+head(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan"))
 
 # Visualising the data 
 (read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan")) %>% 
   ggplot(aes(x=treatment,
-             y=plate))+
+             y=set_up_date))+
   geom_point()
+
+read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f0") %>% 
+  ggplot(aes(x=treatment,
+             y=offspring))+
+  geom_col()
+
+read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1") %>%
+ggplot(aes(x= parental_treatment,
+           y=offsprings))+
+  geom_col()
+
+# Attempt to look at days living for f1/f0 c.elegans
+(read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan"))
+
+f1lifespan_wide <- (read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan")) %>% 
+  pivot_wider(names_from=worm, values_from=worm, id_cols=worm) %>% 
+  mutate(difference=death_date-set_up_date)
+
+difftime(set_up_date, death_date, days)
      
+
+
 
 
 
