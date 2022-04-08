afe5ba07a6b5bf1156632e5a9cb4442732ab64bb
katherinemillar02 <ajh20fhu@uea.ac.uk>
2022-04-08 00:11:06 +0000

    Added code to remove NA values from death date so the code could be read as a date. Created a dataframe which removed the NA values (anything where death date didn't exist).  Used the created dataframe for a boxplot comparing longevity with treatment (dark or light) with target of the ev or raga gene. Tried out different graphs to understand the data and look at if the raga gene effects longevity of the worm. Tidied data by consolidating capitalisation.

--- Spring Summative Code.R
+++ Spring Summative Code.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(readxl)
+library(reshap)
+library(ggplot2)
+library(dplyr)
 
 
 # Importing the sheets
@@ -8,22 +11,45 @@ f1lifespan <- (read_excel(path = "Data/elegans.xlsx", sheet = "f1_lifespan", na 
 f0reproduction <- (read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f0", na = "NA"))
 f1reproduction <- (read_excel(path = "Data/elegans.xlsx", sheet = "reproduction_f1", na = "NA"))
 
+# Lowercase the data to more easily group
+f0lifespan$rnai <- tolower(f0lifespan$rnai)
+f0lifespan$treatment <- tolower(f0lifespan$treatment)
+f1lifespan$parental_rnai <- tolower(f1lifespan$parental_rnai)
+f1lifespan$parental_treatment <- tolower(f1lifespan$parental_treatment)
+f1lifespan$treatment <- tolower(f1lifespan$treatment)
+
+
 
 # Calculating the longevity for f0/1
 f0lifespan$longevity <- as.numeric (difftime
-                                     (f0lifespan$death_date,f0lifespan$set_up_date,
+                                     (f0lifespan$death_date, f0lifespan$set_up_date,
                                        units="days"))
 
 f1lifespan$longevity <- as.numeric (difftime
-                                     (f1lifespan$death_date,f1lifespan$set_up_date,
+                                     (f1lifespan$death_date, f1lifespan$set_up_date,
                                        units="days"))
 
+                                                    
+
+# Comparing longevity and parental treatment
+ggplot(data=f1lifespan, aes(x = tolower(parental_treatment), y = longevity)) +
+  geom_boxplot(aes(fill = longevity),
+               alpha = 0.2, 
+               width = 0.5, 
+               outlier.shape=NA)+
+  geom_jitter(aes(colour = longevity),
+              width=0.2)+
+  theme(legend.position = "none")
 
 
 
 
 
-                             
-                                                                                    
-
-
+# Create a data frame and boxplot comparing longevity with treatment and genes
+f0l <- na.omit(f0lifespan)
+rnai=f0l$rnai
+treatment=f0l$treatment
+longevity=f0l$longevity
+data=data.frame(rnai, treatment ,  longevity)
+ggplot(data, aes(x=rnai, y=longevity, fill=treatment)) + 
+  geom_boxplot()
